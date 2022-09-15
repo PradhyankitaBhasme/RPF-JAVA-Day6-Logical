b1b5c95de1852b9c227a4ec062e17f577b5bec99
Pradhyankita Bhasme <pradhyankita@gmail.com>
2022-09-15 11:36:10 +0530

    Adding: Logical Problems

---
+++ src/com/bridgelabz/logical/StopWatch.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.logical;
+
+//import java.util.Scanner;
+//6. Simulate Stopwatch Program
+//        a. Desc -> Write a Stopwatch Program for measuring the time that elapses between
+//        the start and end clicks
+//        b. I/P -> Start the Stopwatch and End the Stopwatch
+//        c. Logic -> Measure the elapsed time between start and end
+//        d. O/P -> Print the elapsed time.
+
+public class StopWatch {
+        public static void main(String[] args) throws InterruptedException {
+            long startTime, endTime, elapsedTime;
+
+            startTime = System.currentTimeMillis();
+            System.out.println("start time : "+startTime);
+
+            Thread.sleep(3000);
+
+            endTime = System.currentTimeMillis();
+            System.out.println("end time : "+endTime);
+
+            elapsedTime = endTime - startTime;
+            System.out.println("elapsed time = "+elapsedTime);
+        }
+}
